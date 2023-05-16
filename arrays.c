8f6aad3c52523ed2925f7b2e8e20859622f787a5
DeLuks <dragojeviclk16@gmail.com>
2023-05-16 21:19:59 +0200

    modified:   arrays.c

--- arrays.c
+++ arrays.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int c, i, nwhite, nother;
-    int ndigit[10];
+  int c, i, nwhite, nother;
+  int ndigit[10]; // declares ndigit to be an array of 10 integers
 
-    nwhite = nother = 0;
-    for (i = 0; i < 10; ++i) {
-        ndigit[i] = 0;
-    }
+  nwhite = nother = 0;
+  for (i = 0; i < 10; ++i) {
+    ndigit[i] = 0;
+  }
 
-    while ((c = getchar()) != EOF) {
-        if (c >= '0' && c <= 9) {
-            ++ndigit[c-'0'];
-        }
-        else if ( c == ' ' || c == '\n' || c == '\t') {
-            ++nwhite;
-        }
-        else {
-            ++nother;
-        }
+  while ((c = getchar()) != EOF) {
+    if (c >= '0' && c <= '9') { // checks if the character is a digit
+      ++ndigit[c - '0']; // of it is the numeric value of that digit is c - '0'
     }
-
-    printf("digits =");
-    for (i = 0; i < 10; ++i) {
-        printf(", white space = %d, other = %d\n", nwhite, nother);
+    else if (c == ' ' || c == '\n' || c == '\t') {
+      ++nwhite;
     }
-    
-    return 0;
+    else {
+      ++nother;
+    }
+    }
+  printf("\ndigits =");
+  for (i = 0; i < 10; ++i) { 
+    printf(" %d", ndigit[i]);
+  }
+  printf(", white space = %d, other = %d\n", nwhite, nother);
+  return 0;
 }
